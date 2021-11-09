58c7695815ffaba3e47840a60fa71d0148aacda8
FCO-1 <pacomendez012@gmail.com>
2021-11-09 11:21:21 -0600

    funciones de suma

--- lib/sum_numbers.ex
+++ lib/sum_numbers.ex
@@ -1,6 +1,11 @@
 defmodule SumNumbers do
+
   @moduledoc """
   Documentation for `SumNumbers`.
+  Sums 2 numbers.  and returns the result.
+  Example:
+  ```
+    iex> sum_numbers.sum(1, 2)
   """
 
   @doc """
@@ -14,5 +19,24 @@ defmodule SumNumbers do
   """
   def hello do
     :world
+  end
+
+  def sun_numbers(a, b) do
+    sum(a, b)
+  end
+
+  defp sum(a, b)  when a != nil and b != nil do
+    a + b
+  end
+
+
+  defp sum(a,b) when  a == nil do
+    b
+  end
+  defp sum(a,b) when  b == nil do
+    a
+  end
+  defp sum(a,b) when  a == nil and b == nil do
+    0
   end
 end
